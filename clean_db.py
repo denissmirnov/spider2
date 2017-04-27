fb34547c40d22a9526888b90e28295efc2a236f7
Denis Smirnov <denis.smirnov@inplat.ru>
2017-04-27 11:42:34 +0300

    fixes

--- clean_db.py
+++ clean_db.py
@@ -55,8 +55,13 @@ class Cleaner:
             ins_torrent = {}
             torrent_url = []
             for torrent in torrents:
-                ins_torrent = torrent
-                torrent_url += ins_torrent['torrent_url']
+                f = False
+                for t in torrent_url:
+                    if t == torrent:
+                        f = True
+                if not f:
+                    ins_torrent = torrent
+                    torrent_url += ins_torrent['torrent_url']
             ins_torrent['torrent_url'] = torrent_url
             sql = """
                 DELETE from torrents
